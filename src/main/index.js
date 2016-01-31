617466b8a7e80a0a1c5c8767980656b91ca1ba20
Aitor Aznar Álvarez <aitor@aitoraznar.com>
2016-01-31 21:21:45 +0100

    CRON Job Enabled

--- src/main/index.js
+++ src/main/index.js
@@ -3,28 +3,38 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 console.log('Environment:', process.env.NODE_ENV);
 
 var properties = require('../../properties');
-var cron = require('cron');
+var CronJob = require('cron').CronJob;
 var cantabriaService = require('../services/cantabriaService.js');
 var emailService = require('../services/emailService.js');
 
+var cronJob = new CronJob(
+	'* * * * *', // Every 11:00
+	function() {
+		cantabriaService.getFollowUpList(function(err, html) {
+		    var maestrosInfo = cantabriaService.extractInfo(html, properties.cantabria['list-folloup'].types.maestros);
 
+		    var alarm = cantabriaService.checkAlarm(maestrosInfo, properties.cantabria['list-folloup'].currentPosition);
+		    
+			var emailContent = emailService.createEmailContent(maestrosInfo, alarm, properties.cantabria['list-folloup'].currentPosition);
+			emailService.sendEmail(
+				properties.cantabria['list-folloup'].email.from,
+				properties.cantabria['list-folloup'].email.to, 
+				emailContent, function(error, info){
+				    if(error){
+				        return console.log(error);
+				    }
 
-cantabriaService.getFollowUpList(function(err, html) {
-    var maestrosInfo = cantabriaService.extractInfo(html, properties.cantabria['list-folloup'].types.maestros);
+				    console.log('Message sent: ' + info.response);
+				});
 
-    var alarm = cantabriaService.checkAlarm(maestrosInfo, properties.cantabria['list-folloup'].currentPosition);
-    
-	var emailContent = emailService.createEmailContent(maestrosInfo, alarm, properties.cantabria['list-folloup'].currentPosition);
-	emailService.sendEmail(
-		properties.cantabria['list-folloup'].email.from,
-		properties.cantabria['list-folloup'].email.to, 
-		emailContent, function(error, info){
-		    if(error){
-		        return console.log(error);
-		    }
-
-		    console.log('Message sent: ' + info.response);
 		});
-	
-});
+	}, 
+	function(){
+		//finish action
+	}, 
+	true, 
+	'Europe/Madrid');
+
+console.log('CRON Job enabled. Waiting...');
+
 

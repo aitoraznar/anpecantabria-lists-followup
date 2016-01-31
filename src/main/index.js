38b703a88324a01f73fdab94bf26373de51adc80
Aitor Aznar Álvarez <aitor@aitoraznar.com>
2016-01-31 21:12:50 +0100

    The status email is sent every day

--- src/main/index.js
+++ src/main/index.js
@@ -3,25 +3,28 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 console.log('Environment:', process.env.NODE_ENV);
 
 var properties = require('../../properties');
+var cron = require('cron');
 var cantabriaService = require('../services/cantabriaService.js');
 var emailService = require('../services/emailService.js');
+
+
 
 cantabriaService.getFollowUpList(function(err, html) {
     var maestrosInfo = cantabriaService.extractInfo(html, properties.cantabria['list-folloup'].types.maestros);
 
     var alarm = cantabriaService.checkAlarm(maestrosInfo, properties.cantabria['list-folloup'].currentPosition);
-    if (alarm.isAlert || alarm.isWarning) {
-    	var emailContent = emailService.createEmailContent(maestrosInfo, alarm, properties.cantabria['list-folloup'].currentPosition);
-    	emailService.sendEmail(
-    		properties.cantabria['list-folloup'].email.from,
-    		properties.cantabria['list-folloup'].email.to, 
-    		emailContent, function(error, info){
-			    if(error){
-			        return console.log(error);
-			    }
+    
+	var emailContent = emailService.createEmailContent(maestrosInfo, alarm, properties.cantabria['list-folloup'].currentPosition);
+	emailService.sendEmail(
+		properties.cantabria['list-folloup'].email.from,
+		properties.cantabria['list-folloup'].email.to, 
+		emailContent, function(error, info){
+		    if(error){
+		        return console.log(error);
+		    }
 
-			    console.log('Message sent: ' + info.response);
-			});
-    }
+		    console.log('Message sent: ' + info.response);
+		});
+	
 });
 
